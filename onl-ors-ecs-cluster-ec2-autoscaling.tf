67bb67f8bbdf39f44b4439ff6c438111c1775685
sanprasirt <sanprasirt@pccth.com>
2023-05-24 09:35:22 +0700

    update AMI optimize

--- onl-ors-ecs-cluster-ec2-autoscaling.tf
+++ onl-ors-ecs-cluster-ec2-autoscaling.tf
@@ -141,7 +141,8 @@ module "ecs_service" {
 
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html#ecs-optimized-ami-linux
 data "aws_ssm_parameter" "ecs_optimized_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+  # name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2023/recommended"
 }
 
 module "alb_sg" {
