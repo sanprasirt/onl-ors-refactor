54d8e6da080f444d1565928795d1be5c967e5540
Sanprasirt boonma <kuforce@gmail.com>
2023-05-27 09:05:59 +0700

    add new ecr receive and cancel

--- onl-ors-ecr.tf
+++ onl-ors-ecr.tf
@@ -12,3 +12,31 @@ resource "aws_ecr_repository" "onl-ors-reserve" {
     create_before_destroy = true
   }
 }
+
+resource "aws_ecr_repository" "onl-ors-receive" {
+  image_tag_mutability = "MUTABLE"
+  name                 = "${local.prefix}-receive"
+  tags                 = local.common_tags
+
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_ecr_repository" "onl-ors-cancel" {
+  image_tag_mutability = "MUTABLE"
+  name                 = "${local.prefix}-cancel"
+  tags                 = local.common_tags
+
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
--- onl-ors-ecs-cluster-ec2-autoscaling.tf
+++ onl-ors-ecs-cluster-ec2-autoscaling.tf
@@ -311,14 +311,14 @@ module "autoscaling" {
 module "autoscaling_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
-  
+
 
   name        = "${local.prefix}-asg-sg"
   description = "Autoscaling group security group"
   vpc_id      = var.vpc_id
-  
+
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules = [ "ssh-tcp" ]
+  ingress_rules       = ["ssh-tcp"]
   computed_ingress_with_source_security_group_id = [
     {
       rule                     = "http-80-tcp"
